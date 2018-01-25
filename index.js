ec816d45a0fe75161909b25570dbc3212d7a04f6
Precious Humphrey <phumphrey23@yahoo.com>
2018-01-25 05:26:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ function takeANumber(katzDeliLine, name){
   return('Welcome, '+name+'. You are number '+katzDeliLine.length+' in line.');
 }
 function nowServing(katzDeliLine){
-  for(var i=0; i<katzDeliLine.length; i++){
     if(katzDeliLine.length === 0){
     return "There is nobody waiting to be served!";
   } else{
     return('Currently serving '+ katzDeliLine.shift() +'.');
   }
-}
+
 }
 var line = [];
 function currentLine(line){
