915f15af498e94685979aa84809932e86b4d2c75
Precious Humphrey <phumphrey23@yahoo.com>
2018-01-25 05:29:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ function nowServing(katzDeliLine){
   }
 
 }
-var line = [];
+
 function currentLine(line){
     if(katzDeliLine.length === 0){
       return "The line is currently empty.";
   } else{
+    var line = [];
     for (var i = 0; i < katzDeliLine.length; i++){
      line[i] = `${[i+1]}. ${katzDeliLine[i]}`};
      return ('The line is currently:  '+ line)
