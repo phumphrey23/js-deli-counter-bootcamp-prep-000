675e804093b65e977e8cfb08ed18324adfb39659
Precious Humphrey <phumphrey23@yahoo.com>
2018-01-25 05:25:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function nowServing(katzDeliLine){
   }
 }
 }
-function currentLine(katzDeliLine){
-  var line = [];
+var line = [];
+function currentLine(line){
   for(var i=0; i<KatzDeliLine.length; i++){
     if(katzDeliLine.length === 0){
       return "The line is currently empty.";
