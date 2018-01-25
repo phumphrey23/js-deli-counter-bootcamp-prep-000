7252a18b7952c10a9609011a0fe6893d28757f47
Precious Humphrey <phumphrey23@yahoo.com>
2018-01-25 05:09:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var katzDeliLine = [];
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
-  return('Welcome, '+name+'. You are number '+katzDeli.length+' in line.');
+  return('Welcome, '+name+'. You are number '+katzDeliLine.length+' in line.');
 }
 function nowServing(katzDeliLine){
   for(var i=0; i<katzDeliLine.length; i++){
