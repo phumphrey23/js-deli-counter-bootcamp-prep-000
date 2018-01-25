3e0a70edea6a44c7b1653e439130fd67e5d4c7c5
Precious Humphrey <phumphrey23@yahoo.com>
2018-01-25 05:09:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-var katzDeli = [];
-function takeANumber(katzDeli, name){
-  katzDeli.push(name);
+var katzDeliLine = [];
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
   return('Welcome, '+name+'. You are number '+katzDeli.length+' in line.');
 }
-function nowServing(katzDeli){
-  for(var i=0; i<katzDeli.length; i++){
-    if(katzDeli.length === 0){
+function nowServing(katzDeliLine){
+  for(var i=0; i<katzDeliLine.length; i++){
+    if(katzDeliLine.length === 0){
     return "There is nobody waiting to be served!";
   } else{
-    return('Currently serving '+ katzDeli.shift() +'.');
+    return('Currently serving '+ katzDeliLine.shift() +'.');
   }
 }
 }
 function currentLine(line, name){
   var line = [];
-  for(var i=0; i<KatzDeli.length; i++){
-    if(katzDeli.length === 0){
+  for(var i=0; i<KatzDeliLine.length; i++){
+    if(katzDeliLine.length === 0){
       return "The line is currently empty.";
   } else{
     line.push(name)
