4069fd6fc76a93fa5e5da7a2e5ed56cf6863755a
Precious Humphrey <phumphrey23@yahoo.com>
2018-01-25 05:01:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ function nowServing(katzDeli){
   }
 }
 }
-function currentLine(KatzDeli, name){
-  var katzDeliLine = [];
+function currentLine(line, name){
+  var line = [];
   for(var i=0; i<KatzDeli.length; i++){
     if(katzDeli.length === 0){
       return "The line is currently empty.";
   } else{
-    katzDeliLine.push(name)
-    return 'The line is currently:'+ katzDeliLine
+    line.push(name)
+    return 'The line is currently:'+ line
   }
 
   }
